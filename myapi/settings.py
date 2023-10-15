c3b41bd1d99c99088b10b344c54faf33549217e7
karthikeyanJ <karthikeyanj1915@gmail.com>
2023-10-15 10:19:07 +0530

    modified for pythonanywhere

--- myapi/settings.py
+++ myapi/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-6ld9qp@#id(wxzz=r2@o6t&=fyeb791_wckrwf%czx+%f99m!1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,11 +115,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- myapi/urls.py
+++ myapi/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import handler404
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('api.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
